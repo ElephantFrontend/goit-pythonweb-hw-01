747b8d5d7cc6993a7a28807715f5e34ceb7f4846
illia.artiushenko <iartiushenko@deloitte.com>
2025-09-06 23:36:12 +0300

    fixed comment

--- task_1.py
+++ task_1.py
@@ -1,9 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import Type
-import logging
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+from logger import logger
 
 
 class Vehicle(ABC):
